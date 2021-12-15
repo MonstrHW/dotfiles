9ec56197dbfa09fdc908c646f63a62bd1e90490b
MonstrHW <nukolau08@gmail.com>
2021-12-16 03:29:51 +0800

    Add javascript in treesitter

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = {"java", "lua", "bash", "python", "php", "go", "html", "css"},
+	ensure_installed = {"java", "lua", "bash", "python", "php", "go", "html", "css", "javascript"},
 	highlight = {
 		enable = true,
 		disable = { "html", "css" }
