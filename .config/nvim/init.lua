898fd896a3d9d54b1afdc5c18f8ac45d28b0b7a7
MonstrHW <nukolau08@gmail.com>
2022-01-20 21:56:52 +0800

    Add vim-xkbswitch plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -99,5 +99,14 @@ require('packer').startup(
 			'rcarriga/nvim-dap-ui',
 			config = function() require("dapui").setup() end
 		}
+
+		use {
+			'lyokha/vim-xkbswitch',
+			config = function()
+				vim.g.XkbSwitchEnabled = 1
+				vim.g.XkbSwitchLib = '/usr/local/lib/libxkbswitch.so'
+				vim.g.XkbSwitchIMappings = {'ru'}
+			end
+		}
 	end
 )
