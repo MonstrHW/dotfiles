0df1f596046e77cbb986090def0d1d9b2954034c
MonstrHW <nukolau08@gmail.com>
2022-07-01 23:52:39 +0800

    Remove options

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -15,12 +15,7 @@ require('gitsigns').setup {
 		buffer = true,
 	},
 
-	watch_index = {
-		interval = 1000
-	},
-
 	sign_priority = 6,
 	update_debounce = 200,
 	status_formatter = nil,
-	use_decoration_api = false
 }
